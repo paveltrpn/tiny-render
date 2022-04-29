45ecc8d12341298254307f52be40b1e9e79808e1
paveltrpn <paveltrpn@mail.ru>
2022-04-29 18:34:29 +0400

    add image package (local), refactor main.go

--- cmd/software/main.go
+++ cmd/software/main.go
@@ -1,99 +1,95 @@
 package main
 
 import (
-	"image"
-	"image/jpeg"
-	"os"
-	"tiny-render-go/internal/service"
+	img "tiny-render-go/internal/image"
 
 	"github.com/veandco/go-sdl2/sdl"
 )
 
-func loadImage(fname string) image.Image {
-	imagefile, err := os.Open(fname)
-	if err != nil {
-		println(err)
-		panic(0)
-	}
-	defer imagefile.Close()
+type sdlGlobalState_s struct {
+	window   *sdl.Window
+	screen   *sdl.Surface
+	renderer *sdl.Renderer
 
-	imageData, err := jpeg.Decode(imagefile)
-	if err != nil {
-		println(err)
-		panic(0)
-	}
+	wnd_width  int
+	wnd_height int
+	name       string
 
-	return imageData
+	run bool
 }
 
-func imageToRGB(img image.Image) []uint8 {
-	sz := img.Bounds()
-	raw := make([]uint8, (sz.Max.X-sz.Min.X)*(sz.Max.Y-sz.Min.Y)*3)
-	idx := 0
-	for y := sz.Min.Y; y < sz.Max.Y; y++ {
-		for x := sz.Min.X; x < sz.Max.X; x++ {
-			r, g, b, _ := img.At(x, y).RGBA()
-			raw[idx], raw[idx+1], raw[idx+2] = uint8(r), uint8(g), uint8(b)
-			idx += 3
-		}
+func initSdlGlobalState(width, height int, name string) sdlGlobalState_s {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		println("initSdlGlobalState(): ERROR while sdl init!")
+		panic(err)
 	}
-	return raw
+
+	window, err := sdl.CreateWindow(name,
+		sdl.WINDOWPOS_UNDEFINED,
+		sdl.WINDOWPOS_UNDEFINED,
+		int32(width), int32(height), sdl.WINDOW_SHOWN)
+	if err != nil {
+		println("initSdlGlobalState(): ERROR while window creation!")
+		panic(err)
+	}
+
+	surface, err := window.GetSurface()
+	if err != nil {
+		println("initSdlGlobalState(): ERROR while screen sourface creation!")
+		panic(err)
+	}
+
+	surface.FillRect(nil, 100)
+
+	render, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_SOFTWARE)
+	if err != nil {
+		println("initSdlGlobalState(): ERROR while render creation!")
+		panic(err)
+	}
+
+	return sdlGlobalState_s{window: window,
+		screen:     surface,
+		renderer:   render,
+		wnd_width:  width,
+		wnd_height: height,
+		name:       name,
+		run:        true}
+}
+
+func destroySdlGlobalState(state *sdlGlobalState_s) {
+	state.window.Destroy()
+	sdl.Quit()
 }
 
 func main() {
-	// imageData := loadImage("brick-wall.jpg")
+	sdlGlobalState := initSdlGlobalState(800, 600, "sdl")
+	defer destroySdlGlobalState(&sdlGlobalState)
 
-	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
-		panic(err)
-	}
-	defer sdl.Quit()
-
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		800, 600, sdl.WINDOW_SHOWN)
-	if err != nil {
-		panic(err)
-	}
-	defer window.Destroy()
-
-	surface, err := window.GetSurface()
-
-	if err != nil {
-		panic(err)
-	}
-	surface.FillRect(nil, 100)
-
-	cnvs, _ := service.BuildCanvas(512, 512, 3)
+	cnvs, _ := img.BuildCanvas(512, 512, 3)
 	cnvs.BrasenhamLine(10, 10, 512, 512)
 
-	rect := sdl.Rect{0, 0, int32(cnvs.GetWidth()), int32(cnvs.GetHeight())}
-
-	render, _ := sdl.CreateRenderer(window, -1, sdl.RENDERER_SOFTWARE)
-
-	texture, _ := render.CreateTexture(sdl.PIXELFORMAT_RGB24,
+	texture, _ := sdlGlobalState.renderer.CreateTexture(sdl.PIXELFORMAT_RGB24,
 		sdl.TEXTUREACCESS_TARGET, int32(cnvs.GetWidth()), int32(cnvs.GetHeight()))
 	defer texture.Destroy()
-
+	rect := sdl.Rect{0, 0, int32(cnvs.GetWidth()), int32(cnvs.GetHeight())}
 	texture.Update(&rect, cnvs.GetDataPtr(), cnvs.GetWidth()*3)
 
-	window.UpdateSurface()
+	sdlGlobalState.window.UpdateSurface()
 
-	running := true
-	for running {
+	for sdlGlobalState.run {
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
 			switch t := event.(type) {
 			case *sdl.QuitEvent:
-				println("Quit")
-				running = false
+				sdlGlobalState.run = false
 
 			case *sdl.KeyboardEvent:
 				if t.Keysym.Sym == sdl.K_ESCAPE {
-					println("Quit")
-					running = false
+					sdlGlobalState.run = false
 				}
 			}
 		}
 
-		render.Copy(texture, nil, &rect)
-		render.Present()
+		sdlGlobalState.renderer.Copy(texture, nil, &rect)
+		sdlGlobalState.renderer.Present()
 	}
 }
--- internal/image/canvas.go
+++ internal/image/canvas.go
@@ -1,4 +1,4 @@
-package service
+package image
 
 import (
 	"errors"
